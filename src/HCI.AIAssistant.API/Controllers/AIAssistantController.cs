45a97526aef74921a5155107162856b8c177ee40
Bogdan <103338406+bogdanmb04@users.noreply.github.com>
2025-11-14 19:08:50 +0200

    first

---
+++ src/HCI.AIAssistant.API/Controllers/AIAssistantController.cs
@@ -0,0 +1,21 @@
+using HCI.AIAssistant.API.Models.DTOs.AIAssistantController;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HCI.AIAssistant.API.Controllers
+{
+[ApiController]
+[Route("api/[controller]")]
+public class AIAssistantController : ControllerBase
+{
+        [HttpPost("/message")]
+        public async Task<ActionResult<AIAssistantControllerPostMessageResponseDTO>> PostMessage([FromBody] AIAssistantControllerPostMessageRequestDTO request)
+        {
+            AIAssistantControllerPostMessageResponseDTO response = new()
+            {
+                TextMessage = "HI!" + request.TextMessage
+            };
+
+            return Ok(response);
+        }
+}
+}
